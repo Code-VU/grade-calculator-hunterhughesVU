10a599d8a6bbd3eadfa237cde5e613c2b056a93b
Hunter Hughes <Hunter.Hughes@vu.com>
2022-01-25 15:11:07 -0600

    reverted

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -8,15 +8,15 @@ def calculateGrade():
 
         if(score >= 0.9 and score <= 1.0):
             grade = 'A'
-        if(score >= 0.8 and score < 9.0):
+        elif(score >= 0.8 and score < 9.0):
             grade = 'B'
-        if(score >= 0.7 and score < 8.0):
+        elif(score >= 0.7 and score < 8.0):
             grade = 'C'
-        if(score >= 0.6 and score < 7.0):
+        elif(score >= 0.6 and score < 7.0):
             grade = 'D'
-        if(score < 0.6 and score >= 0.0):
+        elif(score < 0.6 and score >= 0.0):
             grade = 'F'
-        if(score < 0.0 or score > 1.0):
+        else:
             grade = "Bad score"
 
     except: grade = 'Bad score'
@@ -24,6 +24,8 @@ def calculateGrade():
     
     print(grade)
         
+
+
     # end assignment
 
 ## if you want to test locally before you try to sync
