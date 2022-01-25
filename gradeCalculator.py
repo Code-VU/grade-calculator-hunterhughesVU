2c03af6b425b2cbfc0e641bc00897bfc52142e03
Hunter Hughes <Hunter.Hughes@vu.com>
2022-01-25 14:54:01 -0600

    Initial commit

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,7 +3,28 @@ def calculateGrade():
     print("Calculating Grade")
     # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
+    try:
+        score = float(input("Enter score:"))
+
+        if(score >= 0.9 and score <= 1.0):
+            grade = 'A'
+        elif(score >= 0.8 and score < 9.0):
+            grade = 'B'
+        elif(score >= 0.7 and score < 8.0):
+            grade = 'C'
+        elif(score >= 0.6 and score < 7.0):
+            grade = 'D'
+        elif(score < 0.6 and score >= 0.0):
+            grade = 'F'
+        else:
+            grade = "Score is out of range! (0.0 - 1.0)"
+
+    except: grade = 'Invalid input!'
+
+    
+    print(grade)
+        
+
 
     # end assignment
 
@@ -11,4 +32,4 @@ def calculateGrade():
 ## uncomment calculateGrade() and run > python calculateGrade.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-## calculateGrade()
+#calculateGrade()
