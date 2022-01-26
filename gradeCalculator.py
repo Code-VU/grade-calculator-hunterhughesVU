7ccf00edd34f2e472653f55660d2e7791a3779bb
Hunter Hughes <Hunter.Hughes@vu.com>
2022-01-26 07:13:37 -0600

    Fixed numbers

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -8,11 +8,11 @@ def calculateGrade():
 
         if(score >= 0.9 and score <= 1.0):
             grade = 'A'
-        elif(score >= 0.8 and score < 9.0):
+        elif(score >= 0.8 and score < 0.9):
             grade = 'B'
-        elif(score >= 0.7 and score < 8.0):
+        elif(score >= 0.7 and score < 0.8):
             grade = 'C'
-        elif(score >= 0.6 and score < 7.0):
+        elif(score >= 0.6 and score < 0.7):
             grade = 'D'
         elif(score < 0.6 and score >= 0.0):
             grade = 'F'
